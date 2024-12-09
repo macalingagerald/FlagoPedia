ba6a516aa81b1509370afa66d061b395fe3ea9c8
Indignus12 <junegerald123@gmail.com>
2024-12-09 13:44:56 -0800

    Initial commit

---
+++ app/src/main/java/com/example/flagopedia/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.flagopedia
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var startBtn: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        startBtn = findViewById(R.id.start_btn)
+
+        startBtn.setOnClickListener {
+            val intent = Intent(this, CountryList::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
